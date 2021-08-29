31dad8e882fdf3053772e018cf7542c7437fed1e
azizulhakim0794 <67514785+azizulhakim0794@users.noreply.github.com>
2021-08-29 16:25:12 +0600

    firebase deploy fix

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://ancient-springs-49674.herokuapp.com"
 })
 export default instance;
